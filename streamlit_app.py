4f04ff591541ab2d75e51d1d7bd2729a8932dbc4
Nikolaj Marsk Andersen <137148077+nimazean@users.noreply.github.com>
2026-05-28 14:05:25 +0200

    Add nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
